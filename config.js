5bc3997148db677c77edbc946a1e82fafa1f95ab
Mozan-mozan <alhyanyassmin141@gmail.com>
2024-09-29 14:17:51 +0100

    Update config.js

--- config.js
+++ config.js
@@ -13,8 +13,8 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-["972546887176", '👑 Flash - Zack 👑', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
-["201224563219", '💫 Flash - Naruto 💫', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
+["212771559702", '👑 موزان - المطور مستر موزان 👑', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
+["212773261791", '💫 موزان - موزان 💫', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
 ]
 
 
